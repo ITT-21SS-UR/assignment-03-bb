b86a43b1625d57fd08a2b575b3b772810bc1bc08
KayBrinkmann <57799266+KayBrinkmann@users.noreply.github.com>
2021-05-03 14:52:45 +0100

    Add files via upload

--- reaction_time.py
+++ reaction_time.py
@@ -11,9 +11,29 @@ FIELDNAMES = ['ID', 'condition', 'shown stimulus', 'pressed key', 'correct key',
 
 class Experiment():
 
+
     def __init__(self):
-        
-        
+        self.ui = uic.loadUi("experiment.ui", self)
+        self.timesDict = {'ID':[], 'condition':[], 'shown stimulus':[], 'pressed key':[], 'correct key':[],
+              'reaction time':[], 'timestamp':[]}
+
+
+    def keyPressEvent(self, ev):
+        if ev.key() == QtCore.Qt.Key_Space:
+            self.counter += 1
+            self.update()
+
+    def start_test(self,testtype):
+        #set explanation text
+        print(testtype)
+
+    def start_reaction(self, waittime):
+
+    def save_results(self,dict):
+        resultsdf = pd.read_csv("results.csv")
+
+
+
 
 
 def main():
