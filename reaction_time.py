3ea4faea43f49d4de73a103859214dc287103756
Joshua Benker <joshuabenker@Joshuas-MBP.fritz.box>
2021-05-05 12:22:50 +0200

    experiment 1/2 done

--- reaction_time.py
+++ reaction_time.py
@@ -7,7 +7,6 @@ import time
 import os.path as path
 
 
-
 class Experiment(QtWidgets.QMainWindow):
 
     def __init__(self):
@@ -30,9 +29,9 @@ class Experiment(QtWidgets.QMainWindow):
         self.experiment_two_done = False
         self.testhandler()
 
+    # checks the state of the experiment and calls the next steps
     def testhandler(self):
         self.ui.startButton.show()
-        print(self.testtype, self.experiment_one_done, self.experiment_two_done)
         if self.testtype == "":
             if self.participant_id % 2:
                 self.explanation_one()
@@ -47,47 +46,41 @@ class Experiment(QtWidgets.QMainWindow):
                 self.show_text_end()
                 self.save_results(self.reactiondict)
                 self.ui.startButton.hide()
-
             else:
                 self.explanation_two()
                 self.testtype = "B"
                 self.experiment_two_done = True
-
         elif self.testtype == "B":
             if self.experiment_one_done:
                 self.show_text_end()
                 self.save_results(self.reactiondict)
                 self.ui.startButton.hide()
-
             else:
                 self.explanation_one()
                 self.testtype = "A"
                 self.experiment_one_done = True
 
-
+    # starts the experiment when the button is clicked
     def start_test(self):
         self.ui.startButton.hide()
         self.counter = 0
         self.start_reaction()
 
+    # sets the id of the participant from the console input
     def set_participantId(self):
         try:
             self.participant_id = int(sys.argv[1])
         except (ValueError, IndexError):
             print("Please insert a number as an argument")
             self.participant_id = 1
-            #sys.exit(3)
 
-
+    # initalise the "start"-button
     def init_button(self):
         self.ui.startButton.clicked.connect(self.start_test)
 
-
-    #if key is pressed while the programm is waiting for a reaction, the reactiontime is saved
+    # if key is pressed while the programm is waiting for a reaction, the reactiontime is saved
     def keyPressEvent(self, ev):
-        print(self.waitforreaction)
         if self.waitforreaction:
-            print(ev.text())
             if ev.text() == "a":
                 self.ui.rightArrowText.hide()
                 self.ui.leftArrowText.hide()
@@ -104,18 +97,13 @@ class Experiment(QtWidgets.QMainWindow):
                 self.counter = self.counter + 1
                 self.start_reaction()
 
-
-
-
-
-
-
+    # explanaiton for the first condition
     def explanation_one(self):
         self.ui.explainText.setText("A left arrow will be shown on the screen. "
                                     "When it appears,\npress the 'a'-key "
                                     "on your keyboard as fast as possible.")
-                                    
 
+    # explanaiton for the second condition
     def explanation_two(self):
         self.ui.explainText.setText("Either a left or right arrow will be shown "
                                     "on the screen. \nPress the 'a'-key "
@@ -124,22 +112,21 @@ class Experiment(QtWidgets.QMainWindow):
                                     "-as fast as possible. \n"
                                     "Press the Button below to start")
 
+    # text when experiment is finished
     def show_text_end(self):
         self.ui.explainText.setText("You are done. Thank you for participating in the "
                                     "experiment.")
 
-
+    # starts the experiment
     def start_reaction(self):
-        if self.counter < 2:
+        if self.counter < 10:
             print("setup wait")
             QtCore.QTimer.singleShot(random.randint(2, 6)*1000, lambda: self.show_stimulus())
         else:
             self.testhandler()
 
-
-
-
-    def show_stimulus (self):
+    # shows the stimulus on the screen depending on the condition
+    def show_stimulus(self):
         self.stimulustarttime = dt.datetime.now().timestamp()
         if self.testtype == "A":
             self.ui.leftArrowText.show()
@@ -153,10 +140,12 @@ class Experiment(QtWidgets.QMainWindow):
                 self.shownkey = "left"
         self.waitforreaction = True
 
+    # returns the reaction time
     def get_reactiontime(self):
         return dt.datetime.now().timestamp() - self.stimulustarttime
 
-    def save_reaction(self, reactiontime = 0.0, pressedkey = "left"):
+    # saves the reaction from the participant to the dictonary
+    def save_reaction(self, reactiontime=0.0, pressedkey="left"):
         self.reactiondict["ID"].append(self.participant_id)
         self.reactiondict["condition"].append(self.testtype)
         self.reactiondict["shown_stimulus"].append(self.shownkey)
@@ -168,21 +157,15 @@ class Experiment(QtWidgets.QMainWindow):
             self.reactiondict["correct_key"].append(False)
         self.reactiondict["timestamp"].append(dt.datetime.now())
 
-
-
-
-    def save_results(self,dict):
+    # saves the results to an .csv file
+    def save_results(self, dict):
         if path.exists("results.csv"):
             resultsdfold = pd.read_csv("results.csv")
             resultsdfnew = pd.DataFrame.from_dict(dict)
             resultsdf = resultsdfold.append(resultsdfnew)
         else:
-            print(dict)
             resultsdf = pd.DataFrame.from_dict(dict)
         resultsdf.to_csv("results.csv", index=False)
-
-
-
 
 
 def main():
@@ -190,7 +173,6 @@ def main():
     experiment = Experiment()
     experiment.show()
     sys.exit(app.exec_())
-
 
 if __name__ == '__main__':
     main()
